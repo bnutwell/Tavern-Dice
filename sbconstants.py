2cf34ec4215e2fcd0a915e75bd2a32493b06a879
bnutwell <bnutwell@gmail.com>
2022-08-09 11:31:04 -0400

    first commit

---
+++ sbconstants.py
@@ -0,0 +1,13 @@
+# Establish high-level control parameters for the Stones and Bones game
+NPLAYERS = 4        # number of initial players
+NROUNDS = 10        # number of rounds in one session
+NSESSIONS = 5       # max number of sessions in the game
+STARTBONUS = 0      # number of bonus stones each player has to start the game
+STARTTBONUS = 0     # number of bonus stones the treasury has to start the game
+STDAWARD = 5        # standard coin award for exceptional results
+STDPENALTY = 5      # standard coin penalty for exceptional results
+STARTBAG = 20       # starting number of coins for each player
+STARTTREASURY = 0   # starting number of coins in treasury
+ROUNDANTE = 1       # amount each player must ante each round
+COMPTYPE = 'MAX'    # 'MAX' for comparing highest rolls, 'SUM' for comparing summed rolls
+NRULES = 18         # number of active game rules
